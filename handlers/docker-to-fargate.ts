42dadd822d100aec46fc7a68a0a42146dd825fd4
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-08-08 09:29:55 +0530

    Create `deployDockerToFargate`

--- handlers/docker-to-fargate.ts
+++ handlers/docker-to-fargate.ts
@@ -1,32 +1,36 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-export function pushToEcrAndEcs() {
+export function deployDockerToFargate() {
+    const cluster = new aws.ecs.Cluster("cluster", {});
+    
+    const lb = new awsx.lb.ApplicationLoadBalancer("lb", {});
+
     const repository = new awsx.ecr.Repository("repository", {});
+    
     const image = new awsx.ecr.Image("image", {
         repositoryUrl: repository.url,
         path: "./src",
     });
-    const cluster = new aws.ecs.Cluster("cluster", {});
-    const lb = new awsx.lb.ApplicationLoadBalancer("lb", {});
+
     const service = new awsx.ecs.FargateService("service", {
         cluster: cluster.arn,
         assignPublicIp: true,
         taskDefinitionArgs: {
             container: {
                 image: image.imageUri,
-                cpu: 512,
-                memory: 128,
+                cpu: 128,
+                memory: 50,
                 essential: true,
                 portMappings: [{
                     targetGroup: lb.defaultTargetGroup,
                 }],
             },
         },
+        desiredCount: 2
     });
-    
-    const appUrl = lb.loadBalancer.dnsName;
 
-    return { appUrl };
+    const loadBalancerUrl = lb.loadBalancer.dnsName;
+
+    return { loadBalancerUrl };
 }
--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import { deployStaticHtmlToS3 } from './handlers/static-html-to-s3'
-import { pushToEcrAndEcs } from './handlers/push-to-ecr-and-ecs'
+import { deployDockerToFargate } from './handlers/docker-to-fargate'
 
 /**
  * Deploy static HTML to S3
  */
 // export const { bucketName, bucketEndpoint } = deployStaticHtmlToS3();
 
+
 /**
- * Push to ECR and ECS
+ * Deploy to Fargate
  */
-export const { appUrl } = pushToEcrAndEcs();
+// export const { loadBalancerUrl } = deployDockerToFargate();
